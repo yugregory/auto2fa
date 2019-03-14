ae8e5ca41f778f1f650bb5b38fa936da17be9ebd
gobisa <gobisa@umich.edu>
2019-03-14 18:02:04 -0400

    fixes for release

--- app/src/main/java/com/example/auto2fa/MainActivity.java
+++ app/src/main/java/com/example/auto2fa/MainActivity.java
@@ -1,21 +1,12 @@
 package com.example.auto2fa;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Color;
 import android.provider.Settings;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
-//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // RECEIVER VARIABLES
@@ -92,15 +83,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /*
-    class NotificationReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.i(TAG,"**********  onReceive");
-            //String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
-            //txtView.setText(temp);
-        }
-    }
-    */
 
 }
--- app/src/main/java/com/example/auto2fa/NotificationListener.java
+++ app/src/main/java/com/example/auto2fa/NotificationListener.java
@@ -26,17 +26,10 @@ public class NotificationListener extends NotificationListenerService{
         if (notificationPackageName.equals(duoPackageName)) {
             try {
                 if (n.actions != null && n.actions.length > 0) {
-                    //Log.i(TAG, "actionIntent: " + n.actions[0].actionIntent.toString());
-                    //for (Notification.Action action : n.actions) {
-                        //Log.i(TAG, "Action's remoteInputs: " + Arrays.toString(action.getRemoteInputs()));
-                    //}
-                    //Log.i(TAG, "Action's remoteInputs: " + n.actions);
                     n.actions[0].actionIntent.send();
-                    //Log.i(TAG, "num actions: " + n.actions.length);
                 }
-                //Log.i(TAG, "num actions: 0");
             } catch (android.app.PendingIntent.CanceledException e) {
-                Log.e(TAG, e.getMessage());
+                //Log.e(TAG, e.getMessage());
             }
         }
 
