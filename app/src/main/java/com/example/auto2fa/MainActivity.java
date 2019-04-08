6d8cb7f734529300a286ab3d2cfd528203e65444
Gregory Yu <gregoryyu@Gregorys-MacBook-Pro.local>
2019-04-08 00:06:16 -0400

    i think i added toggle functionality

--- app/src/main/java/com/example/auto2fa/MainActivity.java
+++ app/src/main/java/com/example/auto2fa/MainActivity.java
@@ -5,8 +5,10 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     // RECEIVER VARIABLES
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = this.getClass().getSimpleName();
     private TextView notificationAccessInstructionsTextView;
     private Button notificationAccessButton;
+    private Button toggleButton;
     private TextView noActionRequiredTextView;
 
     @Override
@@ -22,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationListener listener = new NotificationListener();
+        final NotificationListener listener = new NotificationListener();
 
         notificationAccessButton = findViewById(R.id.grantNotificationAccessButton);
         notificationAccessInstructionsTextView = findViewById(R.id.getNotificationAccessInstructions);
         noActionRequiredTextView = findViewById(R.id.noActionView);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        ToggleButton toggle = (ToggleButton) toggleButton;
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    listener.setChecker(true);
+                } else {
+                    listener.setChecker(false);
+                }
+            }
+        });
 
         // ask user for notification permissions if necessary
         boolean canAccessNotifications = isNotificationAccessEnabled();
@@ -36,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             showSetupComplete();
         }
-
     }
 
 
@@ -56,12 +70,13 @@ public class MainActivity extends AppCompatActivity {
         notificationAccessInstructionsTextView.setVisibility(View.VISIBLE);
         notificationAccessButton.setVisibility(View.VISIBLE);
 
+        toggleButton.setVisibility(View.GONE);
         noActionRequiredTextView.setVisibility(View.GONE);
     }
 
     private void showSetupComplete() {
-        noActionRequiredTextView.setVisibility(View.VISIBLE);
-
+        // noActionRequiredTextView.setVisibility(View.VISIBLE);
+        toggleButton.setVisibility(View.VISIBLE);
         notificationAccessInstructionsTextView.setVisibility(View.GONE);
         notificationAccessButton.setVisibility(View.GONE);
     }
--- app/src/main/java/com/example/auto2fa/NotificationListener.java
+++ app/src/main/java/com/example/auto2fa/NotificationListener.java
@@ -9,36 +9,45 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.Arrays;
 
-public class NotificationListener extends NotificationListenerService{
+public class NotificationListener extends NotificationListenerService {
     private String duoPackageName = "com.duosecurity.duomobile";
     private String duoNotificationTitle = "Duo Mobile";
 
     private String TAG = this.getClass().getSimpleName();
+    private boolean checker = false;
+
+    public void setChecker(boolean value) {
+        checker = value;
+        Log.d(TAG, "checker has been changed to: " + value);
+    }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
+        if (checker) {
+            Log.d(TAG, "hello");
+            String notificationPackageName = sbn.getPackageName();
+            Notification n = sbn.getNotification();
 
-        String notificationPackageName = sbn.getPackageName();
-        Notification n = sbn.getNotification();
-
-        if (notificationPackageName.equals(duoPackageName)) {
-            if (n.extras.containsKey("android.title")) {
-                String notificationTitle = n.extras.get("android.title").toString();
-                if (duoNotificationTitle.equals(notificationTitle)) {
-                    try {
-                        if (n.actions != null && n.actions.length > 0) {
-                            n.actions[0].actionIntent.send();
+            if (notificationPackageName.equals(duoPackageName)) {
+                if (n.extras.containsKey("android.title")) {
+                    String notificationTitle = n.extras.get("android.title").toString();
+                    if (duoNotificationTitle.equals(notificationTitle)) {
+                        try {
+                            if (n.actions != null && n.actions.length > 0) {
+                                n.actions[0].actionIntent.send();
+                            }
+                        } catch (android.app.PendingIntent.CanceledException e) {
+                            //Log.e(TAG, e.getMessage());
                         }
-                    } catch (android.app.PendingIntent.CanceledException e) {
-                        //Log.e(TAG, e.getMessage());
                     }
                 }
             }
         }
-
     }
 }
